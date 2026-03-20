feee983c64047c571385da7964478f11fbf0405f
Olin Osborne III <oosborne@unitehere.org>
2026-03-20 15:07:13 -0400

    feat(frontend): add HTTP error interceptor with ApiError conversion

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,18 +2,21 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideAnimationsAsync(),
     provideRouter(routes),
+    provideHttpClient(withInterceptors([errorInterceptor])),
     providePrimeNG({
       theme: {
         preset: Aura,
